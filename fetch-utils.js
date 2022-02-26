b53f1f3901972ebcad6131f5f5f67d367dd514dd
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-25 17:59:14 -0800

    connecting to supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://wlciphartsabwqqpvnez.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsY2lwaGFydHNhYndxcXB2bmV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU4MzM0MjcsImV4cCI6MTk2MTQwOTQyN30.SzJ2OpOQXHBDB0w9uI8_k8i-XGyJs35OAEAbQbJH3zg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
